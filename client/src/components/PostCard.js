4f0b95f865f5fa5b908a3114109e2a038540f9a7
parkoon <devparkoon@gmail.com>
2020-09-13 13:07:50 +0900

    #3 Displaying Posts

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
+import moment from "moment";
+import { Link } from "react-router-dom";
+
+function PostCard({
+  post: { body, craetedAt, id, username, likeCount, commentCount, likes },
+}) {
+  function likePost() {}
+  function commentPost() {}
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/jenny.jpg"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/post/${id}`}>
+          {moment(craetedAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="violet" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label as="a" basic color="violet" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentPost}>
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label as="a" basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default PostCard;
